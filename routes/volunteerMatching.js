1b628aeeba08739f5c86b06ac2df868cf98817fb
AGarcia2K <136547151+AGarcia2K@users.noreply.github.com>
2024-10-18 12:07:15 -0500

    Revert "um think im done"
    This reverts commit f6acf1bd608cd115fe1c5cb003fc67eda167ae55.

--- routes/volunteerMatching.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { matchVolunteersToEvent } = require('../controllers/volunteerMatchingController');
-
-// Define route to match volunteers
-router.get('/:eventId', matchVolunteersToEvent);
-
-module.exports = router;
