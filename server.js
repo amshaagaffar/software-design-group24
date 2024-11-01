41fd153ba373eaae3acec61dd1d9529234100ac2
AGarcia2K <136547151+AGarcia2K@users.noreply.github.com>
2024-10-31 23:27:30 -0500

    Connected MongoDB to node.js

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-// #file:server.js
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+
+// Connect to MongoDB without deprecated options
+mongoose.connect('mongodb://localhost:27017/test');
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.error(err);
+});
+
+db.on('open', () => {
+    console.log('Database Connected');
+});
 
 // Import login and other necessary routes
 const loginRoutes = require('./routes/login'); // Adjust path as necessary
 const notificationRoutes = require('./routes/notificationRoutes'); // Adjust path as necessary
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
+// app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public')); // Serve static files from the 'public' directory
 
 // Use login routes
 app.use(loginRoutes);
